1503bb0ea3f3f8ef9f5a291570b3c7505f73361f
Michellangello27 <mcornejodurand@byupathway.edu>
2026-01-10 13:43:03 -0500

    Added a Comment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,10 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Ask the user for their first name and last name
+
         Console.Write("What is yor first name? ");
         string firstName = Console.ReadLine();
+
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
+
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
